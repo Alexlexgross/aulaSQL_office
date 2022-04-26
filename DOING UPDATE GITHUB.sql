190d59a8afc4e65ec55e7359c34fd6c3d685b5b7
Alexlexgross <alexlexgross@gmail.com>
2022-04-26 17:20:28 -0300

    terceiro update

---
+++ DOING UPDATE GITHUB.sql
@@ -0,0 +1,26 @@
+https://github.com/Alexlexgross/aulaSQL
+
+git remote add origin git@github.com:Alexlexgross/aulaSQL_office.git
+
+ git status
+
+ git push -u origin main
+
+git remote add origin git@github.com:Alexlexgross/aulaSQL_office.git
+git branch -M main
+git push -u origin main
+
+para salvar uma nova versão
+clear
+ 
+$ git status
+
+$ git add .
+
+$ git status
+
+$ git commit -m "second update"
+
+$ git push
+
+
